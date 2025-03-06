0ef2755a2ff5daa6161938d00a27e73ae13ccb8b
Maximilian Remming <maximilian@remming.fi>
2025-03-06 11:22:55 +0200

    Improve docstring of Context.TODO

--- index.ts
+++ index.ts
@@ -18,11 +18,11 @@ export class TimeoutError extends Error {
 
 export const Context = {
   /**
-   * Create a new completely empty context. It is named `TODO` because in _almost_
-   * all cases you should be using the `withAbort` or `withTimeout` functions to
-   * create a new context.
+   * TODO returns a new empty context. Code should use Context.TODO when it's
+   * unclear which Context to use or it is not yet available (because the
+   * surrounding function has not yet been extended to accept a Context parameter).
    *
-   * @returns An empty context.
+   * @returns A new empty context.
    */
   TODO: (): Context => {
     return {} as Context;
